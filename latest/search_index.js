5f28c66f9fb9c6a66f019c2c35c092ee40eefab0
autodocs <autodocs>
2017-11-26 08:47:32 +0000

    build based on 0b92502

--- latest/search_index.js
+++ latest/search_index.js
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Sched.jl",
     "title": "Usage",
     "category": "section",
-    "text": "See example"
+    "text": "Markdown.parse(\"\"\"\n```\n$(readstring(\"sample/sample.jl\"))\n```\n\"\"\")Download example"
 },
 
 {
