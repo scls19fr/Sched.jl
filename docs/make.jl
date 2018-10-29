d593e2afedf1e6a6855c489ca2d6043021ca6727
Sébastien Celles <s.celles@gmail.com>
2018-10-29 12:01:41 +0100

    Doc

--- docs/make.jl
+++ docs/make.jl
@@ -11,10 +11,5 @@ makedocs(
 )
 
 deploydocs(
-    repo = "github.com/scls19fr/Sched.jl",
-    julia  = "0.6",
-    latest = "master",
-    target = "build",
-    deps = nothing,  # we use the `format = :html`, without `mkdocs`
-    make = nothing,  # we use the `format = :html`, without `mkdocs`
+    repo = "github.com/scls19fr/Sched.jl"
 )
